9451b6dee00eda57a0678fb4af9942ab26c2707c
Jose Sanchez <jparkers0012@gmail.com>
2017-12-27 13:58:46 -0500

    implement geocoding for locations using Google geocode API

--- server/service.js
+++ server/service.js
@@ -2,9 +2,20 @@
 
 const express = require('express');
 const service = express();
+const request = require('superagent');
+
+function getCoordinates(location){
+  return `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_GEO_API_KEY}`;
+}
 
 service.get('/service/:location', (req, res, next) => {
-  res.json({result: req.params.location});
+  request.get(getCoordinates(req.params.location), (err, response) => {
+    if(err){
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.json(response.body.results[0].geometry.location);
+  });
 });
 
 module.exports = service;
