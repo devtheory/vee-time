03e2ffca89cefb14de212a87a218053cea9f48b5
Jose Sanchez <jparkers0012@gmail.com>
2017-12-27 23:29:52 -0500

    implement service announcement with dynamic port

--- bin/run.js
+++ bin/run.js
@@ -1,13 +1,27 @@
 'use strict';
 
 require('dotenv').config();
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
 
-server.listen(4000);
+server.listen(); //select open port
 
 server.on('listening', () => {
   console.log(`Vee-time is listening on ${server.address().port} in ${service.get('env')}`);
+
+  const announce = () => {
+    request.put(`http://localhost:3000/service/time/${server.address().port}`, (err, res) => {
+      if(err){
+        console.log(err);
+        console.log("Error connecting to Vee");
+        return;
+      }
+      console.log(res.body);
+    })
+  }
+  announce();
+  setInterval(announce, 15*1000);
 })
